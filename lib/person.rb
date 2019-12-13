e71fd385616973a20ef14b11ce259942b1d686d1
Michael Fahim <michael_fahim@yahoo.com>
2019-12-13 21:12:34 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,30 @@
+class Person
+
+  def name(name)
+  @name = name
+  end
+
+  def name
+  @name
+  end
+
+  def name=(new_name)
+  @name = new_name
+  end
+
+
+  def job(jobx)
+  @job = jobx
+  end
+
+  def job
+  @job
+  end
+
+  def job=(jobx)
+  @job = jobx
+  end
+
+
+
+end
